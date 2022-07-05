0c5d05fb3f462f7b8a1bbe808e2fb61f804bba70
Quo210 <gabrielrengifo211@gmail.com>
2022-07-04 22:24:45 -0400

    first commit

---
+++ public/index.js
@@ -0,0 +1,17 @@
+// www.thecocktaildb.com/api/json/v1/1/random.php RANDOM 
+
+/*
+For the values on the initial presentation card choosing:
+- Preview
+- drink name
+- first 2 ingredients
+- serving glass
+- alcoholic
+*/
+function makeRouletteDiv(){
+    const div = document.querySelector('div.roulette');
+    let number = 3;
+    for (let i = 0; i < number; i++){
+        document.
+    }
+}
