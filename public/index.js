47f628f7eeeb901d566eb9e833667d388ce299fe
Quo210 <gabrielrengifo211@gmail.com>
2022-07-05 21:09:15 -0400

    factory function makes presentation object

--- public/index.js
+++ public/index.js
@@ -2,16 +2,47 @@
 
 /*
 For the values on the initial presentation card choosing:
-- Preview
-- drink name
-- first 2 ingredients
-- serving glass
-- alcoholic
+- Preview = strDrinkThumb
+- drink name = strDrink
+- first 2 ingredients = strIngredient1, ...
+- serving glass = strGlass
+- alcoholic = strAlcoholic
 */
-function makeRouletteDiv(){
-    const div = document.querySelector('div.roulette');
-    let number = 3;
-    for (let i = 0; i < number; i++){
-        document.
+
+function getRandomDrinks(){
+    return fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(data => {
+        return data.drinks[0]
+    })
+    .catch(error =>{
+        if(error) console.log(error)
+    })
+}
+
+function entrify(obj){
+    return Object.entries(obj)
+}
+
+function getIngredients(entries){
+    return entries
+    .filter( pair => /strIngredient/.test(pair[0]) )
+    .filter( pair => pair[1] != null)
+}
+
+getRandomDrinks()
+.then(response => {
+    return drinkPresentation(response)
+})
+.then(res => console.log(res) )
+.catch(err => console.error(err) )
+
+function drinkPresentation(obj){
+    return {
+        'thumbnail': obj.strDrinkThumb,
+        'name': obj.strDrink,
+        'ingredients': getIngredients(entrify(obj)),
+        'glass': obj.strGlass,
+        'alcohol': obj.strAlcoholic
     }
 }
