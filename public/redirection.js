40b57ef53fe95485d4a2d4d5c1b9161ce72017ef
Quo210 <gabrielrengifo211@gmail.com>
2022-07-10 23:45:46 -0400

    memory for explored drinks is working

--- public/redirection.js
+++ public/redirection.js
@@ -5,7 +5,9 @@ function getCards(){
 const enableCards = getCards();
 enableCards.forEach(element => {
     element.addEventListener('click', _ => {
-        localStorage.setItem('latest',getDrinkName(element));
+        const drinkName = getDrinkName(element);
+        localStorage.setItem('latest',drinkName);
+        storeLocally(drinkName);
         window.location.href = '/extended.html'
     })
 })
@@ -18,3 +20,18 @@ function getDrinkName(target){
 //     localStorage.removeItem('latest');
 //     console.log('localStorage for this website is reset');
 // }
+
+function storeLocally(string){
+    const memory = localStorage.getItem('memory');
+    if(!memory){
+        const newMemory = [string];
+        localStorage.setItem('memory',
+            JSON.stringify(newMemory)
+        );
+    } else {
+        const oldMemory = JSON.parse(memory);
+        if(oldMemory.includes(string)) console.log(`${string} exists.`);
+        else oldMemory.push(string);
+        localStorage.setItem('memory',JSON.stringify(oldMemory))
+    }
+}
